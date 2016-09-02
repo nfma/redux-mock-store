68f3a0ed081400590d1432eacd7edda6926a5c62
Nuno Marques <nmarques@equalexperts.com>
2016-09-02 13:14:32 +0100

    removed modules in typings

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,12 @@
-declare module "redux-mock-store" {
-    import {MockStore} from "redux-mock-store-typings"
-    
-    function createMockStore<T>(middlewares?: Redux.Middleware[]): MockStore<T>
+import {Store} from 'redux'
 
-    export = createMockStore
-}
+export type MockStore<T> = (state?: T) => IStore<T>;
 
-declare module "redux-mock-store-typings" {
-    import {Store} from 'redux'
+export interface IStore<T> extends Store<T> {
+    dispatch(action: any): any
+    getState(): T
+    getActions(): Object[]
+    clearActions(): void
+}   
 
-    export type MockStore<T> = (state?: T) => IStore<T>;
-    
-    export interface IStore<T> extends Store<T> {
-        dispatch(action: any): any
-        getState(): T
-        getActions(): Object[]
-        clearActions(): void
-    }   
-}
-
+export default function createMockStore<T>(middlewares?: Redux.Middleware[]): MockStore<T>
